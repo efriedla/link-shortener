0b5801b3d12e636d4a847a1c09a8d89d1660b2c8
Elizabeth friedland <elizabethfriedland@Elizabeths-MacBook.local>
2017-09-25 12:20:05 -0700

    added user form

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    res.send('Hello Backend!');
+
+  app.get("/", function(req, res) {
+res.render("index", {});
 });
+
+
+
+
 
 var server = app.listen(process.env.PORT || 3000);
 
